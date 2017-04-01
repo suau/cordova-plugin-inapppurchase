4c460a3f55aa1a42b1c2eb03c760bdd3f53f60e9
Su-Au Hwang <suau.hwang@gmail.com>
2017-04-02 03:56:25 +0800

    Fix wrong package name

--- src/android/IabBroadcastReceiver.java
+++ src/android/IabBroadcastReceiver.java
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-package com.example.android.trivialdrivesample.util;
+package com.alexdisler.inapppurchases;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
